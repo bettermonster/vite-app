22f61cc48add16021a6241224c257992ad98269c
1481361061@qq.com <jia..134567>
2023-01-13 18:30:44 +0800

    1

--- src/layout/default/index.vue
+++ src/layout/default/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :class="prefixCls">
     <LayoutHeader />
     <div>
       <LayoutSider />
@@ -14,7 +14,30 @@
 
 <script setup lang="ts">
   import LayoutHeader from './header/index.vue';
+  import LayoutSider from './sider/index.vue';
   import LayoutContent from './content/index.vue';
+
+  import { useDesign } from '/@/hooks/web/useDesign';
+  const { prefixCls } = useDesign('default-layout');
 </script>
 
-<style scoped></style>
+<style lang="less">
+  @prefix-cls: ~'@{namespace}-default-layout';
+
+  .@{prefix-cls} {
+    display: flex;
+    width: 100%;
+    min-height: 100%;
+    background-color: #f1f1f1;
+    flex-direction: column;
+
+    > .ant-layout {
+      min-height: 100%;
+    }
+
+    &-main {
+      width: 100%;
+      margin-left: 1px;
+    }
+  }
+</style>
